03b232e010a46bd2085c20591f1d9b4cbd727a1f
MrWaggyOfficial <157229492+MrWaggyOfficial@users.noreply.github.com>
2024-03-18 23:03:44 +0530

    Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,13 @@
-print("Hello")
-name = input("Write your name: ")
+num1 = float(input("Write the first number: "))
+num2 = float(input("Write the second number: "))
 
-if name == "Chaitanya":
-    print("Hello {name}")
+oprtr = input("To add the numbers press '+'\nTo subtract the numbers press '-'\nTo multiply the numbers press 'x'")
+
+if oprtr == "+" :
+    print("The sum of ",num1,"and",num2,"is",num1 + num2)
+
+if oprtr == "-" :
+    print("The difference of ",num1,"and",num2,"is",num1 - num2)
+
+if oprtr == "x" :
+    print("The product of ",num1,"and",num2,"is",num1 * num2)
