ee0dc5ad858ac37530527575e0c6ed4bf65cb9a1
MrWaggyOfficial <157229492+MrWaggyOfficial@users.noreply.github.com>
2024-03-18 23:05:25 +0530

    Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,8 +1,9 @@
+#Getting numbers to be operated on
 num1 = float(input("Write the first number: "))
 num2 = float(input("Write the second number: "))
-
+#Determining the operation
 oprtr = input("To add the numbers press '+'\nTo subtract the numbers press '-'\nTo multiply the numbers press 'x'")
-
+#committing the operation according to user's input
 if oprtr == "+" :
     print("The sum of ",num1,"and",num2,"is",num1 + num2)
 
